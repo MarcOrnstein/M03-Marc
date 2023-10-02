ca5e694a0103b8c6d7094054a3ca5fa4e60916ba
Marc Hernández Domínguez <mahe2501@proven.local>
2023-10-02 14:58:37 +0200

    salario

---
+++ Nucleo formativo 1/salario/src/main/java/com/mycompany/salario/Salario.java
@@ -0,0 +1,40 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Project/Maven2/JavaApp/src/main/java/${packagePath}/${mainClassName}.java to edit this template
+ */
+
+package com.mycompany.salario;
+import java.util.Scanner;
+
+public class Salario {
+
+    public static void main(String[] args) {
+       int horas;
+       int tarifa = 20;
+       int horasextra = 35;
+       
+       Scanner scanner = new Scanner(System.in);
+       System.out.println("cuantas horas has trabajado?");
+       horas = scanner.next().charAt(0);
+       
+       
+       if (horas >= horasextra)
+               {
+           
+       }
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    }
+    
+}
+
+
+
